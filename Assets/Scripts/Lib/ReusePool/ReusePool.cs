ec4095119c1f347b2e0edb3d9c29929f5e398324
乐俊伟 <316586134@qq.com>
2015-08-12 22:32:30 +0800

    fix shader not exist bug

---
+++ Assets/Scripts/Lib/ReusePool/ReusePool.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Box {
+
+	public class ReusePool : MonoBehaviour {
+
+		public GameObject targetPrefab;
+		[Tooltip("Use self as root if remaining null.")]
+		public GameObject poolRoot;
+
+		public int PoolSize { get { return poolSize; } set { SetPoolSize(value); } }
+
+		List<ReusePool> list = new List<ReusePool>();
+		int poolSize;
+
+		public void SetPoolSize(int size) {
+			if (size <= poolSize) {
+				
+				poolSize = size;
+			}
+			poolSize = size;
+
+		}
+
+		// Use this for initialization
+		void Start() {
+			if (poolRoot == null) {
+				poolRoot = gameObject;
+			}
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+	}
+
+}
